fb7aa4cffea3db63070eb4c9192a9e33240f4034
IT24101577 <it24101577@my.sliit.lk>
2024-08-14 14:52:36 +0530

    IT24101577Lab4Q3.java Added

---
+++ Q3/IT24101577Lab4Q3.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class IT24101577Lab4Q3{
+
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int number = input.nextInt();
+
+		System.out.print((number<0)?"The number is: Negative":
+		(number>0)?"The number is: Positive":
+		"The number is: Zero");
+
+		System.out.println();
+
+			
+
+
+
+	
+
+			
+
+	
+
+			
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+}
